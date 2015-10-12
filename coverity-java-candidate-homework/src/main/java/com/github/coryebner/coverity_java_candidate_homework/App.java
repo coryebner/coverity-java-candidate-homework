abd221394732b9e2ac112d8de3ad22982ea790a5
Cory Ebner <coryebner@gmail.com>
2015-10-11 20:50:48 -0600

    Initial Maven Project

---
+++ coverity-java-candidate-homework/src/main/java/com/github/coryebner/coverity_java_candidate_homework/App.java
@@ -0,0 +1,13 @@
+package com.github.coryebner.coverity_java_candidate_homework;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
